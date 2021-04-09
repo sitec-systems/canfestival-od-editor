fefa16c06b3b7990fe7be4c7080d57f2ec657857
Robert Lehmann <robert.lehmann@sitec-systems.de>
2021-04-09 16:44:33 +0200

    Adding xserver registration to run script

--- docker/run.sh
+++ docker/run.sh
@@ -1,1 +1,16 @@
-docker run -e DISPLAY=:0 -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v /home/lehmrob/usr/src/canfestival-od-editor-python2:/root -it --rm  lehmrob/python2
+#!/bin/sh
+
+
+
+set -x
+
+if [ $# -ge 1 ]; then
+    oddir=$1
+else
+    oddir=$(pwd)/..
+fi
+
+# Before you can run it, you need to allow local connections to the xserver
+xhost +local:$(whoami)
+
+docker run -e DISPLAY=:0 -v $oddir:/oddir -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v $(pwd)/..:/root -it --rm  lehmrob/od-editor
