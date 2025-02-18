0ca0161d35921b734e26d4fb2a7f369e3d5acee3
Robert Lehmann <robert.lehmann@sitec-systems.de>
2025-02-18 04:55:13 +0100

    run.sh: Update run script
    Signed-off-by: Robert Lehmann <robert.lehmann@sitec-systems.de>

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-
-
-
 set -x
 
 if [ $# -ge 1 ]; then
@@ -13,4 +10,6 @@ fi
 # Before you can run it, you need to allow local connections to the xserver
 xhost +local:$(whoami)
 
-docker run -e DISPLAY=:0 -v $oddir:/oddir -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v $(pwd)/..:/root -it --rm  lehmrob/od-editor
+# podman run -it -u 0 -e DISPLAY="$DISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v /home/robert/usr/src/canfestival-od-editor/docker/..:/oddir -v /home/robert/usr/src/canfestival-od-editor/docker/..:/root -it --rm lehmrob/od-editor
+
+podman run -u 0 -e DISPLAY="$DISPLAY" -v $oddir:/oddir -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v $(pwd)/..:/root -it --rm  lehmrob/od-editor
